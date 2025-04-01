52e2ed4a465ee6c7bacc3c23934a40bd2ec6fa0d
OneAwen <33322487+Chengwenwen1@users.noreply.github.com>
2025-04-01 15:46:42 +0800

    Update Monocle2.R

--- Monocle2.R
+++ Monocle2.R
@@ -36,7 +36,7 @@ RA_matrix <- as(as.matrix(obj@assays$RNA@counts),'sparseMatrix')
 p_data <-obj@meta.data
 f_data<-data.frame(row.names=rownames(obj),gene_short_name=rownames(obj))
 
-##构建CDS对象
+##
 pd<-new("AnnotatedDataFrame", data =p_data)
 fd<-new("AnnotatedDataFrame", data =f_data)
 cds <- newCellDataSet(RA_matrix,
@@ -51,16 +51,16 @@ cds <- estimateDispersions(cds)
 
 expressed_genes <- VariableFeatures(obj) ##gene set1 
 ##gene set2
-# disp_table <- dispersionTable(cds) # 挑有差异的
-# unsup_clustering_genes <- subset(disp_table, mean_expression >= 0.5) # 挑表达量不太低的
+# disp_table <- dispersionTable(cds) # 
+# unsup_clustering_genes <- subset(disp_table, mean_expression >= 0.5) # 
 # expressed_genes <- unsup_clustering_genes$gene_id
 
 cds <- setOrderingFilter(cds, expressed_genes)  
 plot_ordering_genes(cds)
 cds <- reduceDimension(cds, max_components = 2, method = 'DDRTree')
 cds <- orderCells(cds)
-#使用root_state参数可以设置拟时间轴的根，如下面的拟时间着色图中可以看出，左边是根。根据state图可以看出，根是State1，若要想把另一端设为根，可以按如下操作
-#cds <- orderCells(cds, root_state = 5) #把State5设成拟时间轴的起始点
+#
+#cds <- orderCells(cds, root_state = 5) 
 p1<- plot_cell_trajectory(cds,color_by="Pseudotime", cell_size =0.3,show_backbone=TRUE)+
   gg.theme 
 p2 <- plot_cell_trajectory(cds,color_by="celltype", cell_size =0.3,show_backbone=TRUE)+
@@ -77,12 +77,12 @@ plot_cell_trajectory(cds, color_by = "State",cell_size =0.3,show_backbone=TRUE)+
   facet_wrap(~State, nrow = 1)+gg.theme
 saveRDS(cds,'./cds.Rdata')
 
-##统计样本在各个分支的比例
+##
 tmp <- table(cds$newgroup2,cds$State) %>% as.matrix()
 tmp <- apply(tmp, 1, function(x){x/sum(x)})
 tmp_anno <- obj@meta.data %>% dplyr::select('newgroup2','SCISS') %>% unique()
 rownames(tmp_anno) <- tmp_anno$newgroup2
-tmp_anno<- tmp_anno[colnames(tmp),]##排序
+tmp_anno<- tmp_anno[colnames(tmp),]##
 anno.col<- list(newgroup2=c("NR-A052009"="#81571e",#"041-011-NR"="blue","044-004-NR"="blue", 
                             "NR-A035004"='#966523',
                             "NR-A098004"="#aa7328",
@@ -95,8 +95,8 @@ anno.col<- list(newgroup2=c("NR-A052009"="#81571e",#"041-011-NR"="blue","044-004
                 )
 labels_row = c("State-1", "State-2", "State-3", "State-4","State-5")
 pheatmap::pheatmap(tmp,
-                   display_numbers = TRUE,         #热图格子中显示相应的数值
-                   number_color = "black",         #字体颜色为黑色
+                   display_numbers = TRUE,      
+                   number_color = "black",       
                    fontsize=10,
                    annotation_col = tmp_anno,
                    annotation_colors = anno.col,
@@ -107,7 +107,7 @@ pheatmap::pheatmap(tmp,
                    cluster_cols = FALSE,angle_col = 90,
                    legend = F)
 
-##统计各个分支样本的比例
+#
 tmp <- table(cds$Response,cds$State) %>% as.matrix()
 tmp <- apply(tmp, 2, function(x){x/sum(x)}) %>% t()
 pdf('./pie.pdf')
@@ -118,7 +118,7 @@ for (i in 1:nrow(tmp)){
 }
 dev.off()
 
-##伪时间的亚群密度图
+#
 ggplot(pData(cds),aes(Pseudotime,colour=newgroup2,fill=newgroup2))+
   geom_density(bw=0.5,size=1,alpha=0.5)+
   scale_color_manual(values = patie.col_m) +
@@ -126,14 +126,14 @@ ggplot(pData(cds),aes(Pseudotime,colour=newgroup2,fill=newgroup2))+
   gg.theme
 
 
-####拟时序相关的基因
+#
 Time_diff <- differentialGeneTest(cds[expressed_genes,], cores = 1, 
                                   fullModelFormulaStr = "~sm.ns(Pseudotime)")
 Time_genes <- Time_diff[order(Time_diff$qval,decreasing = F),'gene_short_name'][1:150]
 p=plot_pseudotime_heatmap(cds[Time_genes,],  show_rownames=T, return_heatmap=T,num_clusters=2,
                         hmcols=colorRampPalette(rev(brewer.pal(n = 11, name ="RdBu")))(100))
 
-##选择每个cluster的基因，并对基因进行注释
+#
 p$tree_row
 clusters <- data.frame(cutree(p$tree_row,k=2))
 clusters[,1] <- as.character(clusters[,1])
@@ -188,7 +188,7 @@ c("IGHG2","IGHV2-5","IGHV1-18","IGHM","IGLC3","IGLC2","IGHG4","ID2","IGHV3-23",
 #	CCL4/CCL3/CD9
 
 
-##选择基因绘制热图
+
 genes<-unique(c('CLEC7A','CCR7','TRIM8','CARD9','SLC44A2','MAP3K3','FLOT1','TLR2',#bulk
                 'BCL10','NOD2','ALPK1','RIPK1','MAP3K14','CFLAR','RELA',#pseudo bulk
                 'RPS3','EDN1','SPHK1','CD27','PELI1','BIRC3','F2R','TSPAN6','CD74',#sc pc1
@@ -198,7 +198,7 @@ genes<-unique(c('CLEC7A','CCR7','TRIM8','CARD9','SLC44A2','MAP3K3','FLOT1','TLR2
 time_inflam_gens <- Time_diff[which(Time_diff$qval<0.05),'gene_short_name'][Time_diff[which(Time_diff$qval<0.05),'gene_short_name'] %in% genes]
 plot_pseudotime_heatmap(cds[time_inflam_gens,], num_clusters=1, show_rownames=T, return_heatmap=T)
 time_inflam_gens
-##基因随细胞状态的改变
+
 plot_genes_in_pseudotime(cds[c("EDN1","BIRC3","CD74","LAPTM5","CD27","RPS3",
                                "PELI1","SPHK1","TSPAN6","F2R" ),],color_by = 'newgroup2',ncol = 5)+
   scale_color_manual(values = patie.col_m) +gg.theme
