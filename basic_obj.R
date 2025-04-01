096034d84153672320f6fdc1305d43abb89425ac
OneAwen <33322487+Chengwenwen1@users.noreply.github.com>
2025-04-01 15:41:22 +0800

    Update basic_obj.R

--- basic_obj.R
+++ basic_obj.R
@@ -17,7 +17,7 @@ type.col <- colorRampPalette(c(brewer.pal(n=3,name = 'Set3'),
 colorRampPalette()(15)
 
 
-big.marker <- c('Cd34','Avp','Prss57',#造血干细胞 HSC
+big.marker <- c('Cd34','Avp','Prss57',#HSC
                 'Klf1','Gypa','Slc4a1',#'CD36','TSPO2',#EPC
                 'Hba1', 'Hbb', #Erythrocytes
                 'Cxcl12','Col6a1','Col1a1','Col3a1',#Stromal cell
@@ -40,32 +40,5 @@ big.marker <- c('Cd34','Avp','Prss57',#造血干细胞 HSC
                 "Il3ra","Lilra4",#pDC
                 'Sdc1','Ighg1','Mzb1'#Plasma cells
 )
-big.marker <- c('CD34','AVP','PRSS57',#造血干细胞 HSC
-                'KLF1','GYPA','SLC4A1',#'CD36','TSPO2',#EPC
-                'HBA1', 'HBB', #Erythrocytes
-                'CXCL12','COL6A1','COL1A1','COL3A1',#Stromal cell
-                'CD3D','CD3E','CD4','CD40LG','SELL',#naive
-                'CCR7','LEF1',#naive
-                'IL7R','TIMP1',#'S100A4','GPR183',#c MEMORY #CCR7low
-                'CXCR3','GZMK','GZMA',#CD4 TEM
-                'CD8A','CD8B',#CD8+ T cells
-                'GZMK',#'GZMH',#CD8 mem
-                'GNLY','GZMB',#CD8 E
-                'KLRF1','KLRC1','NCAM1',#NK cell#gnly'KLRB1',
-                'ZNF683',#NKT
-                'RORC','ZBTB16','SLC4A10','KLRB1',#'TRAV1',#',#MAIT 
-                'IL2RA','FOXP3','CTLA4',#Treg
-                'VPREB1','TCL1A','CD19','CD79A','MS4A1',#B cells
-                'TNFRSF13B',#B memory
-                #'SEC11C','XBP1',#MEMORY B
-                'LYZ', #Myeloid cells
-                'CD14', #classic Monocytes, 
-                #'SEMA6B',
-                'FCGR3A', #non classic Monocytes
-                'RETN','MKI67',#prMono
-                #'ELANE',#gmp
-                'CLEC9A',##cDC1 
-                "CLEC10A",'FCER1A','CD1C',##cDC2
-                "IL3RA","LILRA4",#pDC
-                'SDC1','IGHG1','MZB1'#Plasma cells
-)
+
+
